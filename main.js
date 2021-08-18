6c817d04d45bf3bd82ca30351fc198fb034246da
FOLI <you@example.com>
2021-08-18 13:08:34 +0430

    all functioalnlity working

--- main.js
+++ main.js
@@ -1,3 +1,46 @@
+let number;
+let chances=3
+let c =1;
 function start() {
-    document.getElementsByTagName("DIV")[0].style.opacity="0"
+    if(1)document.getElementsByTagName("DIV")[0].style.opacity="0";
+
+    document.getElementsByTagName("DIV")[0].style.display="none";
+    generate();
+    document.getElementsByClassName('layer1')[0].style.display="flex"
+    document.getElementsByClassName('layer1')[0].style.opacity="1"
+}
+function generate(params) {
+    // number=Math.floor((Math.random() * 90) + 10);
+    number=2;
+}
+function first() {
+
+    if(chances==0){sorry()}
+    else if(chances>1){chances--;
+    let guess=Number(document.getElementById('input1').value)
+    compare(guess);
+    if(guess==number){congrates()}else{
+        if(c==0){document.getElementById("response").innerText="Aww missed again";}
+        if(c==1){document.getElementById("response").innerText="aww missed";c=0;}
+
+    document.getElementsByTagName("span")[0].innerText=String(Number(document.getElementsByTagName("span")[0].innerText)-1)}
+
+}
+else{sorry()}
+}
+function sorry() {
+    document.getElementsByTagName("MAIN")[0].style.display="none"
+    document.getElementsByTagName("DIV")[3].style.display="grid"
+}
+function congrates() {
+    document.getElementsByTagName("MAIN")[0].style.display="none"
+    document.getElementsByTagName("*")[0].style.backgroundColor="white"
+    document.getElementsByTagName("DIV")[2].style.display="grid"
+}
+
+function compare(x) {document.getElementById('compare').style.display="block";
+    if(x>number)document.getElementById('compare').innerText="Go lower"
+    else if(x<number)document.getElementById('compare').innerText="Go upper"
+
+
 }
